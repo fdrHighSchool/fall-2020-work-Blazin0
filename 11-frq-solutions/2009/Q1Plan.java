d676f8b3f2aa86c27e668cb9a9ed7322c08fe223
Blazin0 <Thomash0720@gmail.com>
2020-12-14 22:14:57 -0500

    plan

--- 11-frq-solutions/2009/Q1Plan.java
+++ 11-frq-solutions/2009/Q1Plan.java
@@ -1,5 +1,26 @@
 /**
-class main
+Have a random number generator from 1-6
+A run Occurs when 2 or more consecutive tosses
+of the cube produce the same value
 
+A)
+int[] GetCubeTosses(NumberCube cube, int numTosses)
+cube tosses into array
+numtosses > 0
+return array
 
+B)
+getLongestRun(int[] arr, cubetosses)
+int[] array;
+int run = 0
+int maxnum = 0
+while() (or for loop arr.length)
+  if compare arr[i] with arr[i+1]
+    add to array[run] and run++
+  if arr[i] > maxnum
+  maxnum = arr[i]
+    if run == 0
+      return -1
+    else
+      return array
 */
