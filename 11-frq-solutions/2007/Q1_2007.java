8cf247c0fcac5aed1b43732a3e18a772229594f2
Blazin0 <Thomash0720@gmail.com>
2020-12-14 22:51:08 -0500

    fixed

--- 11-frq-solutions/2007/Q1_2007.java
+++ 11-frq-solutions/2007/Q1_2007.java
@@ -1,24 +1,27 @@
+      //i searched up the isSelfDivisor i didnt know how to individually go through each digit
 import java.util.*;
 public class Q1_2007{
   public static void main(String[] args){
-    Scanner s = new Scanner(System.in);
-    System.out.println("input a positive number");
-    int num = s.nextInt();
-    System.out.println("How many values?");
-    int values = s.nextInt();
-    if(isSelfDivisor(num)){
-        if (values < 0 || num < 0){
-      System.out.println("values or num cant be less than 0");
-    }
-    else{
-      firstNumSelfDivisor(num,values);
-      }
-    }
+     Scanner s = new Scanner(System.in);
+     System.out.println("input a positive number");
+     int num = s.nextInt();
+     System.out.println("How many values?");
+     int values = s.nextInt();
+     if(isSelfDivisor(num)){
+      if (values < 0 || num < 0){
+       System.out.println("values or num cant be less than 0");
+     }
+     else{
+       System.out.println(Arrays.toString(firstNumSelfDivisor(num,values)));
+       }
+     }
   }
-  public static boolean isSelfDivisor(int number){
+  public static boolean isSelfDivisor(int num){
+    int number = num;
+    int holder = 0;
     while (number > 0){
-      //i searched up this up i didnt know how to individually go through each digit 
-      if(number % 10 == 0 || number % (number % 10) != 0){
+
+      if(number % 10 == 0 || num % (number % 10) != 0){
         return false;
       }
       number /= 10;
@@ -28,13 +31,12 @@ public class Q1_2007{
   public static int[] firstNumSelfDivisor(int start, int num){
     int[] arr = new int[num];
     int placement = 0;
-    int number = start+1;//starting number
+    int number = start;//starting number
     while(placement < num){//While Placement is less than array size
       if(isSelfDivisor(number)){
         arr[placement] = number;
-        System.out.println(number);
         placement++;
-      }
+        }
       number++;//increase the number
     }
     return arr;
