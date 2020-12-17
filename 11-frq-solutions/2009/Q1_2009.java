ce5ddec0dd178cc5071fd0bc7672228c610130f2
Blazin0 <Thomash0720@gmail.com>
2020-12-16 20:58:02 -0500

    edit

--- 11-frq-solutions/2009/Q1_2009.java
+++ 11-frq-solutions/2009/Q1_2009.java
@@ -6,9 +6,9 @@ import java.util.*;
     for (int i : toss){
       System.out.println(i);
     }
-    System.out.println(getLongestRun(toss));
   }
   public static int[] getCubeTosses(Q1_2009 cube, int numtosses){
+    //Mr. Hans code
     int[] arr = new int[numtosses];
     for(int i = 0; i < arr.length; i++){
       arr[i]=cube.toss();
@@ -16,23 +16,24 @@ import java.util.*;
     return arr;
   }
   public static int getLongestRun(int[] values){
-    //Mr. Hans code
+    //Also Mr. Hans code
     int maxrunindex=-1;
     int maxrunlength=1;
     int runindex=0, runLength = 1;
     for(int i =0; i<values.length;i++){
       if(values[i] == maxrunlength){
         if(runLength>maxrunlength){
-          maxrunlength = runLength;
+          //if runlength is greater than max
+          maxrunlength = runLength;//maxrun = run
           maxrunindex = runindex;
         }
       }
-      else{
+      else{//Else statement for value[i] == maxrunlength
         runindex=i;
         runLength=1;
       }
     }
-    return maxrunindex;
+    return maxrunindex;// return
   }
   public int toss(){
     return (int)Math.floor(Math.random() * 6)+1;
